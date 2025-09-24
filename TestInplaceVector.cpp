996e9ef370d2f204b76833761c53f148bec4c0a5
Pete Isensee <pkisensee@msn.com>
2025-09-24 16:20:23 -0700

    Tested all ctors

--- TestInplaceVector.cpp
+++ TestInplaceVector.cpp
@@ -16,6 +16,7 @@
 
 #include <cassert>
 #include <iostream>
+#include <print>
 #include <ranges>
 #include <utility>
 #include <vector>
@@ -24,6 +25,7 @@
 #include "Util.h"
 
 using namespace PKIsensee;
+using namespace std::literals;
 
 #ifdef _DEBUG
 #define test(e) assert(e)
@@ -67,10 +69,66 @@ int __cdecl main()
     test( iv[ 1 ] == 42 );
   }
 
+  // iterator ctor
+  {
+    const auto init = { 1, 2, 3 };
+    inplace_vector<int, 4> iv( init.begin(), init.end() );
+    test( iv.size() == 3 );
+    test( iv[ 0 ] == 1 );
+    test( iv[ 1 ] == 2 );
+    test( iv[ 2 ] == 3 );
+  }
 
-  inplace_vector<int,1> v;
-  test( v.unchecked_emplace_back( 42 ) == 42 );
-  test( v.back() == 42 );
+  // range ctor
+  {
+    const auto init = { 1, 2, 3 };
+    inplace_vector<int, 4> iv( std::from_range, init );
+    test( iv.size() == 3 );
+    test( iv.front() == 1 );
+    test( iv.back() == 3 );
+  }
+
+  // copy ctor
+  {
+    inplace_vector<int, 4> iv( 3, 42 );
+    // inplace_vector<int, 5> ivx( iv ); // compiler error
+    inplace_vector<int, 4> iv2( iv );
+    test( iv == iv2 );
+  }
+
+  // move ctor
+  {
+    inplace_vector<int, 4> iv( 3, 42 );
+    inplace_vector<int, 4> iv2( std::move(iv) );
+#pragma warning(push)
+#pragma warning(disable: 26800) // use of a moved object
+    test( iv.empty() );
+#pragma warning(pop)
+    test( iv != iv2 );
+    test( iv2.size() == 3 );
+    test( iv2.capacity() == 4 );
+    test( iv2.front() == 42 );
+    test( iv2.back() == 42 );
+    test( iv2[ 1 ] == 42 );
+  }
+
+  // init list ctor
+  {
+    const auto init = { 1, 2, 3 };
+    inplace_vector<int, 4> iv( init );
+    test( iv.size() == 3 );
+    test( iv.front() == 1 );
+    test( iv.back() == 3 );
+    try
+    {
+      inplace_vector<int, 2> ivx( init ); // throws; ivx too small
+    }
+    catch ( const std::bad_alloc& badAlloc )
+    {
+      test( badAlloc.what() == "bad allocation"s );
+    }
+
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
