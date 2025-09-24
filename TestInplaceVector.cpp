ea5c30e13f55cd8cb47c10f300c7713fbd094159
Pete Isensee <pkisensee@msn.com>
2025-09-24 15:30:20 -0700

    Added tests for default, count and count/value ctors. Fixed bug in insert() function to make rotate parameters consistent. Fixed missing return value in emplace_back.

--- TestInplaceVector.cpp
+++ TestInplaceVector.cpp
@@ -33,6 +33,41 @@ using namespace PKIsensee;
 
 int __cdecl main()
 {
+  // default ctor, size, capacity
+  {
+    inplace_vector<int, 100> iv;
+    test( iv.empty() );
+    test( iv.size() == 0 );
+    test( iv.capacity() == 100 );
+    test( iv.max_size() == 100 );
+    iv.reserve( 10 );
+    test( iv.capacity() == 100 );
+    iv.shrink_to_fit();
+    test( iv.capacity() == 100 );
+    test( iv.size() == 0 );
+  }
+
+  // count ctor, front, back, array access
+  {
+    inplace_vector<int, 4> iv(3);
+    test( iv.size() == 3 );
+    test( iv.capacity() == 4 );
+    test( iv.front() == 0 );
+    test( iv.back() == 0 );
+    test( iv[ 1 ] == 0 );
+  }
+
+  // count/value ctor
+  {
+    inplace_vector<int, 4> iv( 3, 42 );
+    test( iv.size() == 3 );
+    test( iv.capacity() == 4 );
+    test( iv.front() == 42 );
+    test( iv.back() == 42 );
+    test( iv[ 1 ] == 42 );
+  }
+
+
   inplace_vector<int,1> v;
   test( v.unchecked_emplace_back( 42 ) == 42 );
   test( v.back() == 42 );
